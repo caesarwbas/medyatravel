b271455593b7c4c6b2f5cf9581405b30edb55b55
caesarwbas <media.holder2211@gmail.com>
2026-07-13 14:29:12 +0300

    Update assets.ts

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -5,6 +5,8 @@ export const assets = {
     "https://res.cloudinary.com/lclxcm8g/image/upload/v1783898973/ChatGPT_Image_Jul_10_2026_02_57_14_PM_cavnct.png",
   logoSlogan:
     "https://res.cloudinary.com/lclxcm8g/image/upload/v1783899005/Jun_26_2026_01_35_12_PM_gn18iu.png",
+  favicon:
+    "https://res.cloudinary.com/lclxcm8g/image/upload/v1783941973/Gemini_Generated_Image_27jxkk27jxkk27jx_m7v3kj.png",
   partnerLogo:
     "https://res.cloudinary.com/lclxcm8g/image/upload/v1783899048/Screenshot_2026-07-12_190135_tlslpg.png",
   backgrounds: {
